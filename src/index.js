a264e5b2d933183785cc8bfae08b42b85583a4f7
Viktor <viktor4strlsov@gmail.com>
2024-07-07 00:24:23 +0600

    main the end

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import Footer from "./components/Footer/Footer";
+import Footer from './components/Footer/Footer.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
